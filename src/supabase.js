f2f89643f5d2b3f1dee9e77a547527cd3ea7d40d
eacartwright <eacartwright@gmail.com>
2026-04-18 18:15:40 -0400

    move supabase keys to environment variables

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = "https://tuyfzwsxsbkxpshwgxwq.supabase.co"
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR1eWZ6d3N4c2JreHBzaHdneHdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY1MjUzMDEsImV4cCI6MjA5MjEwMTMwMX0.Ufjv1YZ7E360khPC99-dcJ8G2HPkXuY0Twy4AQQJ9r8"
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
